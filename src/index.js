b2e6e8002254a23684bc2a5d506abdc7a7bc7ccc
VINICIUS NASCIMENTO BUZZO <vinicius.nbuzzo@senacsp.edu.br>
2024-03-21 11:46:34 -0300

    projeto criado

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// Criando uma aplicação EXPRESS
+
+const express = require('express'); //Importando
+const path = require('path'); //Importando
+const app = express();
+
+const router = express.Router(); //isso permite que agente crie diferentes URLs e endpoints pai o frontend possa fazer chamadas
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + '/pages/home.html'))
+})
+
+app.use(router) // falar para o app usar o router
+
+app.listen(3333, () => {
+    console.log('Servidor Rodando');
+})
